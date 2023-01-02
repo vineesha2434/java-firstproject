88d8bbe5049149095d88d88c3d665f2746e1a34e
vineesha2434 <121739863+vineesha2434@users.noreply.github.com>
2023-01-02 17:16:58 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+package vishnu;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("My First Project");
+System.out.println("Iam vishnu");
+	}
+
+}
